2bf6273e58547d5fedb1a76508c62ae02dcc3e2a
Georges Duverger <georges.duverger@gmail.com>
2009-11-27 16:47:14 -0500

    Add documentation and remove styles

--- jquery.form.js
+++ jquery.form.js
@@ -1,25 +1,45 @@
 /*
  * Form - jQuery plugin
+ * jQuery plugin for in-line editing (save, edit, and cancel)
  * Author: Georges Duverger
  */
 
 // Methods attached to the jQuery.fn object
 
+// Save a field
+// ex.: $("input#example").save()
+
 jQuery.fn.save = function() {
 	return this.each(function(){
 		var value;
+		
+		// Recover the saved element
 		var element = $(".value[data-for='"+$(this).attr("id")+"']");
-		// Get the value (and hide the input)
+		
+		// Determine which type of input we're trying to save
 		if($(this).is("input[type=text], select")) {
-			value = $(this).hide().val(); // input, select
+			value = $(this).hide().val();
 		} else if($(this).is("input[type=checkbox], input[type=radio]")) {
-			value = $(this).hide().is(":checked") ? $(this).val() : "-"; // checkbox, radio
-		} else { throw "Can't save "+this; }
-		// Save the value
-		if(element.length != 0) { $(element).text(value).show() }
-		else { $(this).hide().after("<span class='value' data-for='"+$(this).attr("id")+"'>"+value+"</span>") }
+			// TODO Make the default false value customizable
+			value = $(this).hide().is(":checked") ? $(this).val() : "-";
+		} else {
+			// TODO Throw more informative errors
+			throw "Can't save "+this;
+		}
+		
+		// Display the saved value
+		if(element.length != 0) {
+			// Insert the value into the "saved element" and show it
+			$(element).text(value).show();
+		} else {
+			// If not existing, create a "saved element" with the correct value
+			$(this).hide().after("<span class='value' data-for='"+$(this).attr("id")+"'>"+value+"</span>");
+		}
 	});
 };
+
+// Switch to edit mode
+// ex.: $("input#example").edit()
 
 jQuery.fn.edit = function() {
 	return this.each(function(){
@@ -27,12 +47,18 @@ jQuery.fn.edit = function() {
 	});
 };
 
+
+// Cancel the current edition
+// ex.: $("input#example").cancel()
+
 jQuery.fn.cancel = function() {
 	return this.each(function(){
 		if($(this).is("input[type=text], select")) {
-			$(this).hide().val($(".value[data-for='"+$(this).attr("id")+"']").show().text()); // input, select
+			// For input and select
+			$(this).hide().val($(".value[data-for='"+$(this).attr("id")+"']").show().text());
 		} else if($(this).is("input[type=checkbox], input[type=radio]")) {
-			$(this).hide().attr("checked", ($(".value[data-for='"+$(this).attr("id")+"']").show().text() == $(this).val())); // checkbox, radio
+			// For checkbox and radio
+			$(this).hide().attr("checked", ($(".value[data-for='"+$(this).attr("id")+"']").show().text() == $(this).val()));
 		} else { throw "Can't save"+this; }
 	});
 };
